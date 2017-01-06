de0a8438a9389a8e12ad7c825db8e0565c5a7afe
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-01-06 13:34:42 +0100

    Add a width prop to thumbnails

--- src/components/Thumbnail/Thumbnail.js
+++ src/components/Thumbnail/Thumbnail.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import classnames from 'classnames';
 
-function Thumbnail({ src, label, circle }) {
+function Thumbnail({ src, label, circle, width }) {
   const className = classnames('ui-thumbnail', {
     'ui-thumbnail-circle': circle,
   });
 
+  const style = {};
+
+  if (width) {
+    style.width = `${width}px`;
+  }
+
   return (
     <div className={className}>
       {label && <div className="ui-thumbnail-label">{label}</div>}
-      <img src={src} role="presentation" />
+      <img src={src} role="presentation" style={style} />
     </div>
   );
 }
@@ -18,6 +24,7 @@ Thumbnail.propTypes = {
   src: React.PropTypes.string.isRequired,
   label: React.PropTypes.string,
   circle: React.PropTypes.bool,
+  width: React.PropTypes.number,
 };
 
 export default Thumbnail;
