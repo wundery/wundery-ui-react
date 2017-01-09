4fe5abbe0f8cef37344e683967f634334fb83d09
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-01-09 17:01:17 +0100

    Add light badge theme and small prop

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -6,10 +6,26 @@ import { Tooltip } from '../Tooltip';
 import { Spinner } from '../Spinner';
 
 function Badge(props) {
-  const { children, margin, padding, state, tooltip, icon, theme, loading, label } = props;
+  const {
+    children,
+    icon,
+    label,
+    loading,
+    margin,
+    padding,
+    small,
+    theme,
+    tooltip,
+  } = props;
 
   const style = spacingStyles({ margin, padding });
-  const className = classnames('ui-badge', `ui-badge-${theme || state}`);
+
+  const size = small ? 'small' : 'default';
+  const className = classnames(
+    'ui-badge',
+    `ui-badge-theme-${theme}`,
+    `ui-badge-size-${size}`,
+  );
 
   function renderChildrenWithIcon() {
     if (icon) {
@@ -34,22 +50,18 @@ function Badge(props) {
 }
 
 Badge.propTypes = {
-  state: React.PropTypes.string.isRequired,
   children: React.PropTypes.node,
-  icon: React.PropTypes.oneOfType([
-    React.PropTypes.bool,
-    React.PropTypes.string,
-  ]),
-  tooltip: React.PropTypes.node,
+  icon: React.PropTypes.oneOfType([React.PropTypes.bool, React.PropTypes.string]),
+  label: React.PropTypes.string,
+  loading: React.PropTypes.bool,
   margin: React.PropTypes.string,
   padding: React.PropTypes.string,
-  theme: React.PropTypes.oneOf(['success', 'danger', 'default', 'info']).isRequired,
-  loading: React.PropTypes.bool,
-  label: React.PropTypes.string,
+  small: React.PropTypes.bool,
+  theme: React.PropTypes.oneOf(['success', 'danger', 'default', 'info', 'light']).isRequired,
+  tooltip: React.PropTypes.node,
 };
 
 Badge.defaultProps = {
-  state: 'default',
   theme: 'default',
 };
 
