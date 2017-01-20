73b2a6860c7b42210a6a38f5481c475639817e46
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-01-20 12:44:59 +0100

    Introduce compact and image props for list item component

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import classnames from 'classnames';
+import { Media, MediaContent, MediaIcon } from '../Media';
+import { Thumbnail } from '../Thumbnail';
 
-function ListItem({ children, onClick, active }) {
+function ListItem(props) {
+  const {
+    active,
+    children,
+    compact,
+    imageSrc,
+    imageWidth,
+    onClick,
+  } = props;
+
   const className = classnames('ui-list-item', {
     'ui-list-item-with-onclick': !!onClick,
     'ui-list-item-active': active,
+    'ui-list-item-compact': compact,
   });
+
+  function renderContent() {
+    if (imageSrc) {
+      return (
+        <Media compact={compact}>
+          <MediaIcon>
+            <Thumbnail src={imageSrc} width={imageWidth} />
+          </MediaIcon>
+          <MediaContent>
+            {children}
+          </MediaContent>
+        </Media>
+      );
+    }
+
+    return children;
+  }
 
   return (
     <div className={className} onClick={onClick}>
-      {children}
+      {renderContent()}
     </div>
   );
 }
@@ -18,6 +47,24 @@ ListItem.propTypes = {
   children: React.PropTypes.node,
   onClick: React.PropTypes.func,
   active: React.PropTypes.bool,
+
+  // Renders a compact mode
+  compact: React.PropTypes.bool,
+
+  // If an image is provided it is inserted as media icon
+  imageSrc: React.PropTypes.string,
+
+  // Image with in pixel
+  imageWidth: React.PropTypes.number,
+};
+
+ListItem.defaultProps = {
+  active: false,
+  children: null,
+  compact: false,
+  imageSrc: null,
+  imageWidth: 40,
+  onClick: null,
 };
 
 export default ListItem;
