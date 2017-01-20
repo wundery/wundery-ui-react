2c067908d8fae5425e2568c43f0501021caf172b
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-01-20 16:48:04 +0100

    Enhance nav component and introduce prefix prop

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import classnames from 'classnames';
-import { GridContainer, GridRow, GridCol } from '../Grid';
 
-const Nav = props => (
-  <div
-    className={classnames('ui-nav', {
-      'ui-nav-fixed': props.fixed,
-      'ui-nav-inverse': props.inverse,
-    })}
-  >
-    <GridContainer fluid>
-      <GridRow>
-        <GridCol>
-          <div className={classnames('ui-nav-navitems')}>
-            {props.children}
-          </div>
-        </GridCol>
-      </GridRow>
-    </GridContainer>
-  </div>
-);
+function Nav({ inverse, children, fixed, prefix }) {
+  const className = classnames('ui-nav', {
+    'ui-nav-fixed': fixed,
+    'ui-nav-inverse': inverse,
+  });
+
+  return (
+    <div className={className}>
+      {prefix && <div className="ui-nav-prefix">{prefix}</div>}
+      <div className={classnames('ui-nav-navitems')}>
+        {children}
+      </div>
+    </div>
+  );
+}
 
 Nav.propTypes = {
   fixed: React.PropTypes.bool.isRequired,
   inverse: React.PropTypes.bool.isRequired,
   children: React.PropTypes.node,
+  prefix: React.PropTypes.node,
 };
 
 
 Nav.defaultProps = {
   fixed: false,
   inverse: false,
+  prefix: null,
+  children: null,
 };
 
 export default Nav;
