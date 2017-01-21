dd785aa2e250a57b813236f9c5fe75397b258c48
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-01-21 17:32:52 +0100

    Sidebar: add responsive helper markup

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,19 +1,57 @@
 import React from 'react';
 import classnames from 'classnames';
+import { Icon } from '../Icon';
 
-function Sidebar({ children, embedded }) {
-  const className = classnames('ui-sidebar', { 'ui-sidebar-embedded': embedded });
+class Sidebar extends React.Component {
 
-  return (
-    <div className={className}>
-      {children}
-    </div>
-  );
+  static propTypes = {
+    children: React.PropTypes.node,
+    embedded: React.PropTypes.bool,
+    expandLabel: React.PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    children: null,
+    embedded: false,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // This is just for responsive mode and used by CSS. Has no direct
+      // effect on the UI
+      expanded: false,
+    };
+  }
+
+  onExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  }
+
+  render() {
+    const { children, embedded, expandLabel } = this.props;
+    const { expanded } = this.state;
+
+    const className = classnames('ui-sidebar', {
+      'ui-sidebar-embedded': embedded,
+      'ui-sidebar-expanded': expanded,
+    });
+
+    return (
+      <div className={className}>
+        <div className="ui-sidebar-expand-wrapper">
+          <button onClick={this.onExpandClick}>
+            <Icon name="bars" />
+            {expandLabel}
+          </button>
+        </div>
+        <div className="ui-sidebar-items">
+          {children}
+        </div>
+      </div>
+    );
+  }
 }
-
-Sidebar.propTypes = {
-  children: React.PropTypes.node,
-  embedded: React.PropTypes.bool,
-};
 
 export default Sidebar;
