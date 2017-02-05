f0e15c5fac9fe76e5166c36d5474d9a3ea1f4c01
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-02-05 20:20:23 +0100

    Hint: enable custom icon or no icon at all

--- src/components/Hint/Hint.js
+++ src/components/Hint/Hint.js
@@ -2,26 +2,27 @@ import React from 'react';
 import { Icon } from '../Icon';
 import { Badge } from '../Badge';
 
-const Hint = (props) => {
-  const { children, label } = props;
-
-  return (
-    <div className="ui-hint">
-      <div className="ui-hint-label">
-        <Badge theme="success">
-          <Icon name="lightbulb-o " /> {label}
-        </Badge>
-      </div>
-      <div className="ui-hint-content">
-        {children}
-      </div>
+const Hint = ({ children, label, icon }) => (
+  <div className="ui-hint">
+    <div className="ui-hint-label">
+      <Badge theme="success">
+        {icon && <Icon name={icon} />} {label}
+      </Badge>
     </div>
-  );
-};
+    <div className="ui-hint-content">
+      {children}
+    </div>
+  </div>
+);
 
 Hint.propTypes = {
   children: React.PropTypes.node,
   label: React.PropTypes.string.isRequired,
+  icon: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.bool]),
+};
+
+Hint.defaultProps = {
+  icon: 'lightbulb-o',
 };
 
 export default Hint;
