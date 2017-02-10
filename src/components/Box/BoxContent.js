16acf992a3d4a7ea6cc5a44c6b2aa4e7fec31827
Philipp Alexander Frank <philipp.alexander.frank@gmail.com>
2017-02-10 15:13:50 +0100

    Box content: introduce theme prop

--- src/components/Box/BoxContent.js
+++ src/components/Box/BoxContent.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import classnames from 'classnames';
 
-function BoxContent({ noPadding, transparent, borderBottom, children }) {
+function BoxContent(props) {
+  const {
+    borderBottom,
+    children,
+    noPadding,
+    theme,
+    transparent,
+  } = props;
+
+  const className = classnames('ui-box-content', `ui-box-content-theme-${theme}`, {
+    'ui-box-content-no-padding': noPadding,
+    'ui-box-content-transparent': transparent,
+    'ui-box-content-border-bottom': borderBottom,
+  });
+
   return (
-    <div
-      className={classnames('ui-box-content', {
-        'ui-box-content-no-padding': noPadding,
-        'ui-box-content-transparent': transparent,
-        'ui-box-content-border-bottom': borderBottom,
-      })}
-    >
+    <div className={className}>
       {children}
     </div>
   );
 }
 
 BoxContent.propTypes = {
-  children: React.PropTypes.node,
-  transparent: React.PropTypes.bool,
-  noPadding: React.PropTypes.bool,
   borderBottom: React.PropTypes.bool,
+  children: React.PropTypes.node,
+  noPadding: React.PropTypes.bool,
+  theme: React.PropTypes.oneOf(['default', 'shade']).isRequired,
+  transparent: React.PropTypes.bool,
+};
+
+BoxContent.defaultProps = {
+  borderBottom: false,
+  children: null,
+  noPadding: false,
+  theme: 'default',
+  transparent: false,
 };
 
 export default BoxContent;
